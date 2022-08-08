73f7a0c40cb379d0fc199c99e37177de4caae38b
Alexander Brimblecombe <brimblecombea@gmail.com>
2022-08-08 20:20:32 +1000

    pre-rendering tweaks

--- Site.Wasm/Program.cs
+++ Site.Wasm/Program.cs
@@ -4,7 +4,10 @@ using Site.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+}

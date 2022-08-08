9e56e034e7a6f1512883b38a0c12a9a400edd061
Alexander Brimblecombe <brimblecombea@gmail.com>
2022-08-08 19:16:20 +1000

    updates

--- Site.Wasm/Program.cs
+++ Site.Wasm/Program.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Site.Wasm;
-using Site.Wasm.Config;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// register services
-ServiceRegistry.RegisterServices(builder.Services);
-
 await builder.Build().RunAsync();

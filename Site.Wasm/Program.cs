dfb1a94623dfe92404f2876956974dc363ba1463
Alexander Brimblecombe <alfie@3cssoftware.com.au>
2023-03-18 23:34:30 +1000

    update to net7 and begin refactoring

--- Site.Wasm/Program.cs
+++ Site.Wasm/Program.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Site.Wasm;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
 ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
+await builder.Build().RunAsync();
 
+
+// To facilitate BlazorWasmPreRendering.Build
+// https://github.com/jsakamoto/BlazorWasmPreRendering.Build#services-registration
 static void ConfigureServices(IServiceCollection services, string baseAddress)
 {
     services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
